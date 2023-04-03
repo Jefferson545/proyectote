ebbaf51edb13f79a4ebf2b225c82c5d69312e484
Jefferson545 <jefferson.avelar@catolica.edu.sv>
2023-04-02 23:57:33 -0600

    todito

--- app/src/main/java/com/example/proyectocasifull/Activity_compra.java
+++ app/src/main/java/com/example/proyectocasifull/Activity_compra.java
@@ -5,14 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
 public class Activity_compra extends AppCompatActivity {
     Button btn_compra;
+   // String driveFileUrl = "https://drive.google.com/uc?id=123456abcdefg&export=download";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,7 +64,18 @@ public class Activity_compra extends AppCompatActivity {
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // Cierra el cuadro de diálogo
-                        dialog.dismiss();
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Activity_compra.this);
+                        builder.setMessage("Descargar pdf de terminos y condiciones.");
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Cierra el cuadro de diálogo
+
+
+                                dialog.dismiss();
+                            }
+                        });
+                        AlertDialog dialo = builder.create();
+                        dialo.show();
                     }
                 });
                 AlertDialog dialo = builder.create();
@@ -75,6 +101,7 @@ public class Activity_compra extends AppCompatActivity {
         dialog.show();
 
 
-
     }
+
+
 }
